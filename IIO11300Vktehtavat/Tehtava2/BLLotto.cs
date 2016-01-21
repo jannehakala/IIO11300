f4f1b0ac43b19790f3cad63f9773a72cbac37ff4
jannehakala <janne.hakala94@hotmail.fi>
2016-01-21 17:50:52 +0200

    Finish Tehtava2

---
+++ IIO11300Vktehtavat/Tehtava2/BLLotto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT.IIO11300 {
+    class BLLotto{
+        Random random = new Random();
+        public int[] drawLottoArray(int numbers, int max) {
+            int tempRand;
+            int[] lottoArray = new int[numbers];
+
+            for (int i = 0; i < numbers; i++) {
+                tempRand = random.Next(1, max + 1);
+                lottoArray[i] = tempRand;
+            }
+            Array.Sort(lottoArray);
+            return lottoArray;
+        }
+
+        public string drawGame(string gamename) {
+            switch (gamename) {
+                case "Lotto":
+                    return "Numerot: " + string.Join(", ", drawLottoArray(7, 39));
+                case "Viking Lotto":
+                    return "Numerot: " + string.Join(", ", drawLottoArray(6, 48));
+                case "Eurojackpot":
+                    return "Numerot: " + string.Join(", ", drawLottoArray(5, 50)) + " + " + string.Join(", ", drawLottoArray(2, 8));
+                default:
+                    return "Select game first.";
+            }
+        }
+
+    }
+}
--- IIO11300Vktehtavat/Tehtava2/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/Tehtava2/MainWindow.xaml.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+* Copyright (C) JAMK/IT/Esa Salmikangas
+* This file is part of the IIO11300 course project.
+* Created: 21.1.2016
+* Authors: Janne Hakala
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,26 +20,37 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Tehtava2
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
+namespace JAMK.IT.IIO11300 {
+
+    public partial class MainWindow : Window {
+        public MainWindow() {
             InitializeComponent();
         }
-
-        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
+        private void comboSelectGame_Loaded(object sender, RoutedEventArgs e) {
+            comboSelectGame.Items.Add("Lotto");
+            comboSelectGame.Items.Add("Viking Lotto");
+            comboSelectGame.Items.Add("Eurojackpot");
         }
 
-        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
+        private void btnDraw_Click(object sender, RoutedEventArgs e) {
+            try {
+                txtRandomlyDrawnNumbers.Text = String.Empty;
+                BLLotto lotto = new BLLotto();
+                for (int i = 0; i < int.Parse(txtNumberOfDrawns.Text); i++) {
+                    txtRandomlyDrawnNumbers.AppendText(lotto.drawGame(comboSelectGame.Text));
+                    txtRandomlyDrawnNumbers.AppendText(Environment.NewLine);
+                }
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Select number of drawns.");
+            }
+        }
+        private void btnClear_Click(object sender, RoutedEventArgs e) {
+            txtRandomlyDrawnNumbers.Text = String.Empty;
+        }
 
+        private void btnClose_Click(object sender, RoutedEventArgs e) {
+            Application.Current.Shutdown();
         }
     }
 }

70970539140bf5e529b18e60de47a6cc0952d853
jannehakala <janne.hakala94@hotmail.fi>
2016-01-29 02:15:34 +0200

    Do Tehtava3, multiple rows not currently working

---
+++ IIO11300Vktehtavat/Tehtava3/BLLotto.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace JAMK.IT.IIO11300 {
+    class BLLotto {
+        #region variables
+        Random random = new Random();
+        #endregion
+        #region methods
+        public int[] DrawLottoArray(int numbers, int max) {
+            int tempRand;
+            int[] lottoArray = new int[numbers];
+
+            for (int i = 0; i < numbers; i++) {
+
+                tempRand = random.Next(1, max + 1);
+                if (lottoArray.Contains(tempRand)) {
+                    i--;
+                } else {
+                    lottoArray[i] = tempRand;
+                }
+            }
+            Array.Sort(lottoArray);
+            return lottoArray;
+        }
+
+        public string DrawGame(string gamename, int rownumber) {
+            switch (gamename) {
+                case "Lotto":
+                    return "Row " + rownumber + ": " + String.Join(", ", DrawLottoArray(7, 39));
+                case "Viking Lotto":
+                    return "Row " + rownumber + ": " + String.Join(", ", DrawLottoArray(6, 48));
+                case "Eurojackpot":
+                    return "Row " + rownumber + ": " + String.Join(", ", DrawLottoArray(5, 50)) + " + " + String.Join(", ", DrawLottoArray(2, 8));
+                default:
+                    return "Select game first.";
+            }
+        }
+
+        public void WriteLottoNumbers(string numbers) {
+            string lotto = "Lottorivit" + GetWeekNumber() + ".txt";
+            StreamWriter file = new StreamWriter(lotto);
+
+            StringReader drawnNumbers = new StringReader(numbers);
+            string line = string.Empty;
+            do {
+                line = drawnNumbers.ReadLine();
+                if (line != null) {
+                    string[] array = Regex.Split(line, @"\D+");
+                    file.WriteLine(String.Join(" ", array.Skip(2)));
+                }
+            } while (line != null);
+            file.Close();
+        }
+        public int[] ReadLottoNumbers(string correctrow) {
+            string lotto = "Lottorivit" + GetWeekNumber() + ".txt";
+            StreamReader sr = new StreamReader(lotto);
+            string line = "";
+            int counter = 0;
+            int lineCount = File.ReadLines(lotto).Count();
+            Console.WriteLine(lineCount);
+            int[] correctNumbers = new int[lineCount];
+            string[] array1 = correctrow.Split(' ');
+            StreamReader file = new StreamReader(lotto);
+            while ((line = file.ReadLine()) != null) {
+                string[] array2 = line.Split(' ');
+                for (int i = 0; i < lineCount; i++) {
+                    counter = 0;
+                    foreach (string one in array1) {
+                        foreach (string two in array2) {
+                            if (one == two) {
+                                counter++;
+                                correctNumbers[i] = counter;
+                            }
+                        }
+                    }
+                    
+
+                }
+                
+                
+            }
+            for (int j = 0; j < correctNumbers.Length; j++) {
+                Console.WriteLine(correctNumbers[j]);
+            }
+            return correctNumbers;
+        }
+        public string GetWeekNumber() {
+            var culture = CultureInfo.GetCultureInfo("cs-CZ");
+            var dateTimeInfo = DateTimeFormatInfo.GetInstance(culture);
+            var dateTime = DateTime.Today;
+            int weekNumber = culture.Calendar.GetWeekOfYear(dateTime, dateTimeInfo.CalendarWeekRule, dateTimeInfo.FirstDayOfWeek);
+            string result = "";
+            if (weekNumber < 10) {
+                result = "0" + weekNumber.ToString();
+            } else {
+                result = weekNumber.ToString();
+            }
+            return result;
+        }
+        #endregion
+    }
+}
---
+++ IIO11300Vktehtavat/Tehtava3/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿/*
+* Copyright (C) JAMK/IT/Esa Salmikangas
+* This file is part of the IIO11300 course project.
+* Created: 28.1.2016
+* Authors: Janne Hakala
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JAMK.IT.IIO11300 {
+
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+        private void comboSelectGame_Loaded(object sender, RoutedEventArgs e) {
+            comboSelectGame.Items.Add("Lotto");
+            comboSelectGame.Items.Add("Viking Lotto");
+            comboSelectGame.Items.Add("Eurojackpot");
+        }
+
+        private void btnDraw_Click(object sender, RoutedEventArgs e) {
+            try {
+                txtRandomlyDrawnNumbers.Text = String.Empty;
+                BLLotto lotto = new BLLotto();
+                for (int i = 0; i < int.Parse(txtNumberOfDrawns.Text); i++) {
+                    txtRandomlyDrawnNumbers.AppendText(lotto.DrawGame(comboSelectGame.Text, i+1));
+                    txtRandomlyDrawnNumbers.AppendText(Environment.NewLine);
+                }
+            } catch (Exception ex) {
+                MessageBox.Show("Select number of drawns.");
+            }
+        }
+        private void btnClear_Click(object sender, RoutedEventArgs e) {
+            txtRandomlyDrawnNumbers.Text = String.Empty;
+            txtNumberOfDrawns.Text = String.Empty;
+            txtMatchedNumbers.Text = String.Empty;
+            txtCorrectRow.Text = String.Empty;
+        }
+        private void btnSave_Click(object sender, RoutedEventArgs e) {
+                BLLotto lotto = new BLLotto();
+                lotto.WriteLottoNumbers(txtRandomlyDrawnNumbers.Text);    
+        }
+        private void btnCheckRows_Click(object sender, RoutedEventArgs e) {
+            txtMatchedNumbers.Text = String.Empty;
+            BLLotto lotto = new BLLotto();
+            string text = txtCorrectRow.Text.ToString();
+            int[] array = lotto.ReadLottoNumbers(text);
+            for (int i = 0; i < array.Length; i++) {
+                txtMatchedNumbers.AppendText("Row " + (i + 1) + ": right numbers: " + array[i] + "\n");
+            }
+        }
+        private void btnClose_Click(object sender, RoutedEventArgs e) {
+            Application.Current.Shutdown();
+        }
+    }
+}
+

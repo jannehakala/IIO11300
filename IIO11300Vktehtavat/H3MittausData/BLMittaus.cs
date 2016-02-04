b51939b31e850ae34d2890437888808e0eb3a9ce
jannehakala <janne.hakala94@hotmail.fi>
2016-02-04 11:00:23 +0200

    Seialize H3

--- IIO11300Vktehtavat/H3MittausData/BLMittaus.cs
+++ IIO11300Vktehtavat/H3MittausData/BLMittaus.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace JAMK.IT.IIO11300 {
+    [Serializable()]
     public class MittausData {
+        #region PROPERTIES
         private string kello;
         public string Kello {
             get { return kello; }
@@ -17,6 +20,7 @@ namespace JAMK.IT.IIO11300 {
             get { return mittaus; }
             set { mittaus = value; }
         }
+        #endregion
         #region CONSTRUCTORS
         //luokalle tehdään kaksi konstruktoria
         public MittausData() {
@@ -28,10 +32,48 @@ namespace JAMK.IT.IIO11300 {
             this.mittaus = mdata;
         }
         #endregion
+        #region METHODS
         //ylikirjoitetaan ToString
         public override string ToString() {
             //return base.ToString();
             return kello + "=" + mittaus;
         }
+        public static void SaveToFile(string filename, List<MittausData> datat) {
+            try {
+                StreamWriter sw = File.AppendText(filename);
+                
+                foreach (var item in datat) {
+                    sw.WriteLine(item);
+                }
+                sw.Close();
+            } catch (Exception ex) {
+                throw ex;
+            }
+        }
+        public static List<MittausData> ReadFromFile(string filename) {
+            try {
+                if (File.Exists(filename)) {
+                    MittausData md;
+                    List<MittausData> luetut = new List<MittausData>();
+                    string rivi = "";
+                    StreamReader sr = File.OpenText(filename);
+                    while ((rivi = sr.ReadLine()) != null) {
+                        if (rivi.Length > 3 && rivi.Contains("=")) {
+                            string[] split = rivi.Split('=');
+                            md = new MittausData(split[0], split[1]);
+                            luetut.Add(md);
+                        }
+                    }
+                    sr.Close();
+                    return luetut;
+                } 
+                else {
+                    throw new FileNotFoundException();
+                }
+            } catch(Exception ex) {
+                throw ex;
+            }
+        }
+        #endregion
     }
 }
--- IIO11300Vktehtavat/H3MittausData/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/H3MittausData/MainWindow.xaml.cs
@@ -19,6 +19,8 @@ namespace H3MittausData {
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window {
+
+        List<MittausData> mitatut = new List<MittausData>();
         public MainWindow() {
             InitializeComponent();
             IniMyStuff();
@@ -32,7 +34,71 @@ namespace H3MittausData {
             //luodaan uusi mittausdata olio ja näytetään se käyttäjälle
             MittausData md = new MittausData(txtClock.Text, txtData.Text);
 
-            lbData.Items.Add(md);
+            //lbData.Items.Add(md);
+            mitatut.Add(md);
+            ApplyChanges();
+        }
+        private void ApplyChanges() {
+            lbData.ItemsSource = null;
+            lbData.ItemsSource = mitatut;
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e) {
+            try {
+                //kirjoitetaan mitatut tiedostoon 
+                MittausData.SaveToFile(txtFileName.Text, mitatut);
+                MessageBox.Show("Tiedot tallennettu onnistuneesti tiedostoon " + txtFileName.Text);
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnRead_Click(object sender, RoutedEventArgs e) {
+            try {
+                //kirjoitetaan mitatut tiedostoon 
+                mitatut = MittausData.ReadFromFile(txtFileName.Text);
+                ApplyChanges();
+                MessageBox.Show("Tiedot haettu onnistuneesti tiedostosta " + txtFileName.Text);
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSerialize_Click(object sender, RoutedEventArgs e) {
+            try {
+                JAMK.IT.IIO11300.Serialisointi.SerialisoiXml(txtFileName.Text, mitatut);
+            } 
+            catch(Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void btnDeSerialize_Click(object sender, RoutedEventArgs e) {
+            try {
+                mitatut = JAMK.IT.IIO11300.Serialisointi.DeSerialisoiXml(txtFileName.Text);
+                ApplyChanges();
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSerializeBin_Click(object sender, RoutedEventArgs e) {
+            try {
+                JAMK.IT.IIO11300.Serialisointi.Serialisoi(txtFileName.Text, mitatut);
+            } 
+            catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnDeSerializeBin_Click(object sender, RoutedEventArgs e) {
+            try {
+                object obj = new object();
+                JAMK.IT.IIO11300.Serialisointi.DeSerialisoi(txtFileName.Text, ref obj);
+                mitatut = (List<MittausData>)obj;
+                ApplyChanges();
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }

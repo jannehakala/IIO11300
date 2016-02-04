dc4d3cf956d930786af537b82f3e1910185d64ef
jannehakala <janne.hakala94@hotmail.fi>
2016-02-04 17:35:14 +0200

    Start Tehtava4

---
+++ IIO11300Vktehtavat/Tehtava4/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JAMK.IT.IIO11300 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+        }
+        private void comboSelectTeam_Loaded(object sender, RoutedEventArgs e) {
+            cbTeam.Items.Add("Blues"); cbTeam.Items.Add("HIFK"); cbTeam.Items.Add("HPK");
+            cbTeam.Items.Add("Ilves"); cbTeam.Items.Add("JYP"); cbTeam.Items.Add("KalPa");
+            cbTeam.Items.Add("KooKoo"); cbTeam.Items.Add("Kärpät"); cbTeam.Items.Add("Lukko");
+            cbTeam.Items.Add("Pelicans"); cbTeam.Items.Add("SaiPa"); cbTeam.Items.Add("Sport");
+            cbTeam.Items.Add("Tappara"); cbTeam.Items.Add("TPS"); cbTeam.Items.Add("Ässät");
+        }
+
+
+        private void btnNewPlayer_Click(object sender, RoutedEventArgs e) {
+            try {
+                if (!string.IsNullOrEmpty(txtFirstname.Text) && !string.IsNullOrEmpty(txtLastname.Text)
+                        && !string.IsNullOrEmpty(txtPrice.Text) && !string.IsNullOrEmpty(cbTeam.Text)) {
+                    Pelaaja pelaaja = new Pelaaja(txtFirstname.Text, txtLastname.Text, int.Parse(txtPrice.Text), cbTeam.Text);
+                    listBox.Items.Add(pelaaja.Previewname);
+                    tbStatus.Text = "Tila: Pelaajan luonti onnistui!";
+                } else {
+                    tbStatus.Text = "Tila: Jokin kohta jäi tyhjäksi!";
+                }
+            } catch (Exception ex) {
+                tbStatus.Text = "Tila: Syötä hinnaksi vain numeroita!";
+            }
+            
+        }
+
+        private void btnSavePlayer_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void btnDeletePlayer_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void btnWritePlayers_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e) {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ IIO11300Vktehtavat/Tehtava4/Pelaaja.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JAMK.IT.IIO11300 {
+    public class Pelaaja {
+        #region PROPERTIES
+
+        private string fname;
+        public string Fname {
+            get { return fname; }
+            set { fname = value; }
+        }
+
+        private string lname;
+        public string Lname {
+            get { return lname; }
+            set { lname = value; }
+        }
+
+        public string Fullname {
+            get { return lname + fname; }
+        }
+
+        public string Previewname {
+            get { return fname + " " + lname + ", " + team; }
+        }
+
+        private string team;
+        public string Team {
+            get { return team; }
+            set { team = value; }
+        }
+        
+        private int price;
+        public int Price {
+            get { return price; }
+            set { price = value; }
+        }
+        #endregion
+        #region CONSTRUCTORS
+        public Pelaaja() { }
+        public Pelaaja(string fname, string lname, int price, string team) {
+            this.fname = fname;
+            this.lname = lname;
+            this.price = price;
+            this.team = team;
+        }
+        #endregion
+        #region METHODS
+
+        #endregion
+    }
+}

d7bd603772c53fd325bab1e0bad9d762c10f8b4e
jannehakala <janne.hakala94@hotmail.fi>
2016-02-11 20:30:34 +0200

    Finish Tehtava5A

---
+++ IIO11300Vktehtavat/Tehtava5A/MainWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JAMK.IT.IIO11300 {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        List<Pelaaja> pelaajat;
+        public MainWindow() {
+            InitializeComponent();
+            pelaajat = new List<Pelaaja>();
+            string[] teams = new string[] { "Blues", "HIFK", "HPK", "Ilves", "JYP", "KalPa", "KooKoo",
+            "Kärpät", "Lukko", "Pelicans", "SaiPa", "Sport", "Tappara", "TPS", "Ässät" };
+            cbTeam.ItemsSource = teams;
+        }
+
+        private void btnNewPlayer_Click(object sender, RoutedEventArgs e) {
+            try {
+                if (!string.IsNullOrWhiteSpace(txtFirstname.Text) && !string.IsNullOrWhiteSpace(txtLastname.Text)
+                    && !string.IsNullOrWhiteSpace(txtPrice.Text) && cbTeam.SelectedItem != null) {
+                    if (Regex.IsMatch(txtFirstname.Text, @"^[a-zA-Z]+$") && Regex.IsMatch(txtLastname.Text, @"^[a-zA-Z]+$")) {
+                        Pelaaja pelaaja = new Pelaaja(txtFirstname.Text, txtLastname.Text, int.Parse(txtPrice.Text), cbTeam.SelectedValue.ToString());
+                        if (pelaajat.Exists(Pelaaja => Pelaaja.Fullname == pelaaja.Fullname)) {
+                            pelaaja = null;
+                            tbStatus.Text = "Tila: Pelaaja on jo listassa!";
+                        } else {
+                            pelaajat.Add(pelaaja);
+                            ApplyChanges();
+                            tbStatus.Text = "Tila: Pelaajan luonti onnistui!";
+                        }
+                    } else {
+                        tbStatus.Text = "Tila: Merkkijono on väärässä muodossa.";
+                    }
+                } else {
+                    tbStatus.Text = "Tila: Jokin kenttä jäi tyhjäksi.";
+                }
+            } catch (Exception ex) {
+                tbStatus.Text = "Tila: Merkkijono on väärässä muodossa.";
+            }
+        }
+        private void btnSavePlayer_Click(object sender, RoutedEventArgs e) {
+            try {
+                if (!string.IsNullOrWhiteSpace(txtFirstname.Text) && !string.IsNullOrWhiteSpace(txtLastname.Text)
+                    && !string.IsNullOrWhiteSpace(txtPrice.Text) && cbTeam.SelectedItem != null) {
+                    if (Regex.IsMatch(txtFirstname.Text, @"^[a-zA-Z]+$") && Regex.IsMatch(txtLastname.Text, @"^[a-zA-Z]+$")) {
+                        int index = listBox.Items.IndexOf(listBox.SelectedItem);
+                        pelaajat[index].Fname = txtFirstname.Text;
+                        pelaajat[index].Lname = txtLastname.Text;
+                        pelaajat[index].Team = cbTeam.SelectedValue.ToString();
+                        pelaajat[index].Price = int.Parse(txtPrice.Text);
+                        Clear();
+                        ApplyChanges();
+                        tbStatus.Text = "Pelaaja " + pelaajat[index].Fullname + " tallennettu";
+                    } else {
+                        tbStatus.Text = "Tila: Merkkijono on väärässä muodossa.";
+                    }
+                } else {
+                    tbStatus.Text = "Tila: Jokin kenttä jäi tyhjäksi.";
+                }
+            } catch (Exception ex) {
+                tbStatus.Text = "Tila: Merkkijono on väärässä muodossa.";
+            }
+        }
+        private void btnDeletePlayer_Click(object sender, RoutedEventArgs e) {
+            try {
+                int index = listBox.Items.IndexOf(listBox.SelectedItem);
+                pelaajat.RemoveAt(index);
+                Clear();
+                ApplyChanges();
+                tbStatus.Text = "Tila: Pelaaja poistettu.";
+            } catch (Exception ex) {
+                tbStatus.Text = "Tila: " + ex.ToString();
+            }
+        }
+        private void btnWritePlayers_Click(object sender, RoutedEventArgs e) {
+            try {
+                if (Pelaaja.WriteToFile(pelaajat)) {
+                    tbStatus.Text = "Tila: Pelaajat kirjoitettu tiedostoon.";
+                } else {
+                    tbStatus.Text = "Tila: Pelaajia ei tallennettu tiedostoon.";
+                }
+            } catch (Exception ex) {
+                tbStatus.Text = "Tila: " + ex.ToString(); 
+            }
+        }
+        private void btnReadPlayers_Click(object sender, RoutedEventArgs e) {
+            try {
+                var kaikki = pelaajat.Concat(Pelaaja.ReadFromFile()).ToList();
+                pelaajat = kaikki;
+                listBox.ItemsSource = null;
+                listBox.ItemsSource = pelaajat;
+                tbStatus.Text = "Tila: Pelaajat haettu tiedostosta.";
+            } catch (Exception ex) {
+                tbStatus.Text = "Tila: Haku peruttu tai haussa tapahtui virhe.";
+            }
+        }
+        private void lbSelection_Click(object sender, SelectionChangedEventArgs e) {
+            if (listBox.SelectedItem != null) {
+                int index = listBox.Items.IndexOf(listBox.SelectedItem);
+                txtFirstname.Text = pelaajat[index].Fname;
+                txtLastname.Text = pelaajat[index].Lname;
+                txtPrice.Text = pelaajat[index].Price.ToString();
+                cbTeam.SelectedValue = pelaajat[index].Team;
+                tbStatus.Text = "Tila: " + pelaajat[index].Fullname + " valittu.";
+            }
+        }
+        private void ApplyChanges() {
+            listBox.ItemsSource = null;
+            listBox.ItemsSource = pelaajat;
+        }
+        private void Clear() {
+            txtFirstname.Text = "";
+            txtLastname.Text = "";
+            txtPrice.Text = "";
+            cbTeam.SelectedValue = "";
+        }
+        private void btnClose_Click(object sender, RoutedEventArgs e) {
+            Application.Current.Shutdown();
+        }
+    }
+}
+
---
+++ IIO11300Vktehtavat/Tehtava5A/Pelaaja.cs
@@ -0,0 +1,163 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace JAMK.IT.IIO11300 {
+    [Serializable]
+    public class Pelaaja {
+
+        #region VARIABLES
+        private string fname;
+        private string lname;
+        private string team;
+        private int price;
+        #endregion
+        #region PROPERTIES
+        public string Fname {
+            get { return fname; }
+            set { fname = value; }
+        }
+        public string Lname {
+            get { return lname; }
+            set { lname = value; }
+        }
+        public string Fullname {
+            get { return fname + " " + lname; }
+        }
+        public string Previewname {
+            get { return Fullname + ", " + team; }
+        }
+        public string Team {
+            get { return team; }
+            set { team = value; }
+        }
+        public int Price {
+            get { return price; }
+            set { price = value; }
+        }
+        #endregion
+        #region CONSTRUCTORS
+        public Pelaaja() { }
+        public Pelaaja(string fname, string lname, int price, string team) {
+            this.fname = fname;
+            this.lname = lname;
+            this.price = price;
+            this.team = team;
+        }
+        #endregion
+        #region METHODS
+
+        public static bool WriteToFile(List<Pelaaja> pelaajat) {
+            try {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.FileName = "Pelaajat";
+
+                sfd.InitialDirectory = "d:\\";
+                sfd.Filter = "Tekstitiedostot .txt|*.txt|Xml-tiedostot|*.xml|All files|*.*";
+                Nullable<bool> result = sfd.ShowDialog();
+
+                if (result == true) {
+                    string filename = sfd.FileName;
+                    string terminal = Path.GetExtension(filename);
+                    if (terminal == ".txt") {
+                        StreamWriter sw;
+                        try {
+                            string line = "";
+                            sw = new StreamWriter(filename);
+                            for (int i = 0; i < pelaajat.Count(); i++) {
+                                line = pelaajat[i].GetValues();
+                                sw.WriteLine(line);
+                            }
+                            sw.Close();
+                            return true;
+                        } catch (Exception ex) {
+                            throw ex;
+                        }
+                    }
+                    if (terminal == ".xml") {
+                        XmlWriter file;
+                        try {
+                            file = XmlWriter.Create(filename);
+                            XmlSerializer serializer = new XmlSerializer(pelaajat.GetType());
+                            serializer.Serialize(file, pelaajat);
+                            file.Close();
+                            return true;
+                        } catch (Exception ex) {
+                            throw ex;
+                        }
+                    } else {
+                        return false;
+                    }
+                }
+            } catch (Exception ex) {
+                throw ex;
+            }
+            return false;
+        }
+        public static List<Pelaaja> ReadFromFile() {
+            try {
+                List<Pelaaja> pelaajat = null;
+
+                OpenFileDialog ofd = new OpenFileDialog();
+
+                ofd.InitialDirectory = "d:\\";
+                ofd.Filter = "Tekstitiedostot .txt|*.txt|Xml-tiedostot .xml|*.xml|All files|*.*";
+                Nullable<bool> result = ofd.ShowDialog();
+
+                if (result == true) {
+                    string filename = ofd.FileName;
+                    string terminal = Path.GetExtension(filename);
+
+                    if (terminal == ".txt") {
+                        if (File.Exists(filename)) {
+                            pelaajat = new List<Pelaaja>();
+                            StreamReader sr;
+                            string line;
+                            try {
+                                sr = new StreamReader(File.OpenRead(filename));
+                                while (!sr.EndOfStream) {
+                                    line = sr.ReadLine();
+                                    string[] array = line.Split(' ');
+                                    int price = int.Parse(array[2]);     
+                                    pelaajat.Add(new Pelaaja(array[0], array[1], price, array[3]));
+                                }
+                            } catch (Exception ex) {
+                                throw ex;
+                            }
+                        }
+                    }
+                    if (terminal == ".xml") {
+                        if (File.Exists(filename)) {
+                            pelaajat = new List<Pelaaja>();
+                            XmlReader file;
+                            try {
+                                file = XmlReader.Create(filename);
+                                XmlSerializer serializer = new XmlSerializer(pelaajat.GetType());
+                                pelaajat = serializer.Deserialize(file) as List<Pelaaja>;
+                                file.Close();
+                            } catch (Exception ex) {
+                                throw ex;
+                            }
+                        }
+                    }
+                }
+                return pelaajat;
+            } catch (Exception ex) {
+                throw ex;
+            }
+        }
+        public string GetValues() {
+            return Fullname + " " + Price + " " + Team;
+        }
+        public override string ToString() {
+            return Previewname;
+        }
+        #endregion
+    }
+}

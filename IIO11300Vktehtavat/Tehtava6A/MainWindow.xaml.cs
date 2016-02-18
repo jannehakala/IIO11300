cf53c12cc915435a9021863d50ecd27907fcaf67
jannehakala <janne.hakala94@hotmail.fi>
2016-02-18 22:54:25 +0200

    Continue Tehtava6A

--- IIO11300Vktehtavat/Tehtava6A/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/Tehtava6A/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace Tehtava6A {
     /// <summary>
@@ -20,9 +23,42 @@ namespace Tehtava6A {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-        }
+            tbPath.Text = "aa";
+            try {
+                XmlDocument doc = new XmlDocument();
+                doc.Load("Viinit1.xml");
+                XmlNodeList colorList = doc.SelectNodes("/viinikellari/wine/maa");
+                cbWines.Items.Add("");
 
+                foreach (XmlNode name in colorList) {
+                    if (!cbWines.Items.Contains(name.InnerText)) {
+                        cbWines.Items.Add(name.InnerText);
+                    }
+                }
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        
         private void btnReadWines_Click(object sender, RoutedEventArgs e) {
+            try {
+                XDocument xDoc = XDocument.Load("Viinit1.xml");
+                XmlDocument xml = new XmlDocument();
+                xml.Load("Viinit1.xml");
+                string selected = cbWines.Text.ToString();
+                var items = xDoc.Descendants("wine").Where(w => w.Element("maa").Value == selected);
+                foreach (var item in items) {
+                    dataGrid.ItemsSource = null;
+                  
+                    
+
+                    dataGrid.Items.Add("dsf");
+                    Console.WriteLine(item.Element("nimi").Value + item.Element("maa").Value);
+                } 
+            } catch (Exception ex) {
+
+                MessageBox.Show(ex.Message);
+            }
 
         }
     }

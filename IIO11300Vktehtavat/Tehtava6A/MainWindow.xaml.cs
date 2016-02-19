12960a5097b95b513df9f5e807471c9df8ba72d9
jannehakala <janne.hakala94@hotmail.fi>
2016-02-19 22:40:16 +0200

    Continue Tehtava6A, country selection not working

--- IIO11300Vktehtavat/Tehtava6A/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/Tehtava6A/MainWindow.xaml.cs
@@ -15,6 +15,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml;
 using System.Xml.Linq;
+using System.Configuration;
+
 
 namespace Tehtava6A {
     /// <summary>
@@ -23,12 +25,13 @@ namespace Tehtava6A {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-            tbPath.Text = "aa";
+            tbPath.Text = ConfigurationManager.AppSettings["file"];
+            cbWines.SelectedIndex = 0;
             try {
                 XmlDocument doc = new XmlDocument();
-                doc.Load("Viinit1.xml");
+                doc.Load(tbPath.Text);
                 XmlNodeList colorList = doc.SelectNodes("/viinikellari/wine/maa");
-                cbWines.Items.Add("");
+                cbWines.Items.Add("Kaikki");
 
                 foreach (XmlNode name in colorList) {
                     if (!cbWines.Items.Contains(name.InnerText)) {
@@ -39,27 +42,25 @@ namespace Tehtava6A {
                 MessageBox.Show(ex.Message);
             }
         }
-        
+
         private void btnReadWines_Click(object sender, RoutedEventArgs e) {
             try {
-                XDocument xDoc = XDocument.Load("Viinit1.xml");
-                XmlDocument xml = new XmlDocument();
-                xml.Load("Viinit1.xml");
-                string selected = cbWines.Text.ToString();
-                var items = xDoc.Descendants("wine").Where(w => w.Element("maa").Value == selected);
-                foreach (var item in items) {
-                    dataGrid.ItemsSource = null;
-                  
-                    
-
-                    dataGrid.Items.Add("dsf");
-                    Console.WriteLine(item.Element("nimi").Value + item.Element("maa").Value);
-                } 
+                if (cbWines.SelectedIndex == 0) {
+                    XmlDataProvider xdpWineData = (XmlDataProvider)this.FindResource("WineData");
+                    xdpWineData.Source = new Uri(tbPath.Text, UriKind.RelativeOrAbsolute);
+                } else {
+                    XDocument xDoc = XDocument.Load(tbPath.Text);
+                    string selected = cbWines.Text.ToString();
+                    var items = xDoc.Descendants("wine").Where(w => w.Element("maa").Value == selected);
+                    foreach (var item in items) {
+                        //dataGrid.ItemsSource = null;
+                       // dataGrid.Items.Add("dsf");
+                        Console.WriteLine(item.Element("nimi").Value + item.Element("maa").Value);
+                    }
+                }
             } catch (Exception ex) {
-
                 MessageBox.Show(ex.Message);
             }
-
         }
     }
 }

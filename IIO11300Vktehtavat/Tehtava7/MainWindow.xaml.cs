a531c28d651bef157ce7148b0dfc6845a72001d3
jannehakala <janne.hakala94@hotmail.fi>
2016-02-25 16:35:50 +0200

    Finish Tehtava7

--- IIO11300Vktehtavat/Tehtava7/MainWindow.xaml.cs
+++ IIO11300Vktehtavat/Tehtava7/MainWindow.xaml.cs
@@ -22,18 +22,48 @@ namespace Tehtava7 {
         public MainWindow() {
             InitializeComponent();
         }
-        int numberOfUpperChars;
 
-        private void txtTextToCheck_TextChanged(object sender, TextChangedEventArgs e) {
-            if(txtTextToCheck.Text.Any(c => char.IsUpper(c))){
+        private void txtPasswordToCheck_TextChanged(object sender, RoutedEventArgs e) {
+            string password = txtPasswordToCheck.Password;
+            int characters = password.Count();
+            int numberOfUpperChars = password.Count(char.IsUpper);
+            int numberOfLowerChars = password.Count(char.IsLower);
+            int numberOfDigits = password.Count(char.IsNumber);
+            int numberOfSpecials = Regex.Matches(password, "[^a-zA-Z0-9]").Count;
 
+            int types = 0;
+            if (numberOfUpperChars > 0) types++;
+            if (numberOfLowerChars > 0) types++;
+            if (numberOfDigits > 0) types++;
+            if (numberOfSpecials > 0) types++;
+
+            if (characters == 0) {
+                tbResult.Text = "give password";
+                tbResult.Background = new SolidColorBrush(Colors.Gray);
             }
-            if (Regex.IsMatch(txtTextToCheck.Text, "[A-Z]")) {
-                numberOfUpperChars++;
-            }        
-            
-            tbCharCount.Text = "Characters: " + txtTextToCheck.Text.Length;
+            else if(characters < 8 && characters > 0 && types >= 1) {
+                tbResult.Text = "bad";
+                tbResult.Background = new SolidColorBrush(Colors.Red);
+            }
+            else if(characters < 12 && characters >= 8 && types >= 2) {
+                tbResult.Text = "fair";
+                tbResult.Background = new SolidColorBrush(Colors.Yellow);
+            }
+            else if(characters < 16 && characters >= 12 && types >= 3) {
+                tbResult.Text = "moderate";
+                tbResult.Background = new SolidColorBrush(Colors.LightGreen);
+            }
+            else if(characters >= 16 && types == 4) {
+                tbResult.Text = "good";
+                tbResult.Background = new SolidColorBrush(Colors.Green);
+            }
+
+            tbCharCount.Text = "Characters: " + password.Length;
             tbUpperCaseCount.Text = "Upper case: " + numberOfUpperChars.ToString();
+            tbLoverCaseCount.Text = "Lower case: " + numberOfLowerChars.ToString();
+            tbNumberCount.Text = "Digits: " + numberOfDigits.ToString();
+            tbSpecialCharCount.Text = "Specials: " + numberOfSpecials.ToString();
         }
+        
     }
 }

3b28dada97b2c776a5d4ef2df9985ebce151b709
jannehakala <janne.hakala94@hotmail.fi>
2016-03-17 10:56:22 +0200

    H10 ADOBlanco

---
+++ IIO11300Vktehtavat/IIO13200-ADONET-DEMO-master/DBPlacebo2014.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace JAMK.ICT.Data {
+    public static class DBPlacebo {
+        public static DataTable GetTestCustomers() {
+            //create
+            DataTable dt = new DataTable();
+            //columns
+            dt.Columns.Add("asioid", typeof(string));
+            dt.Columns.Add("LastName", typeof(string));
+            dt.Columns.Add("FirstName", typeof(string));
+            //rows
+            dt.Rows.Add("A3581", "Waltari", "Mika");
+            dt.Rows.Add("B3553", "King", "Stephen");
+            dt.Rows.Add("C1238", "Neruda", "Pablo");
+            dt.Rows.Add("D9876", "Oksanen", "Sofi");
+            return dt;
+        }
+        public static DataTable GetAllCustomersFromSQLServer(string connectionStr, string taulu, string cityfilter, out string viesti) {
+            // basic principle: connect - execute query - disconnect
+            try {
+                SqlConnection myConn = new SqlConnection(connectionStr);
+                myConn.Open();
+                string sql = "";
+                if (cityfilter != "") {
+                    sql = string.Format("SELECT * FROM {0} WHERE city like '{1}'", taulu, cityfilter); 
+                } else {
+                   sql = "SELECT * FROM " + taulu;
+                }
+                SqlCommand cmd = new SqlCommand(sql, myConn);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds, taulu);
+                viesti = "Tiedot haettu onnistuneesti tietokannasta " + myConn.DataSource;
+                return ds.Tables[taulu];
+            } catch (Exception ex) {
+                viesti = ex.Message;
+                throw;
+            }
+        }
+        public static DataTable GetCitiesFromSQLServer(string connectionStr, string taulu) {
+            try {
+                SqlConnection myConn = new SqlConnection(connectionStr);
+                myConn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT DISTINCT city * FROM " + taulu, myConn);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                da.Fill(ds, taulu);
+                return ds.Tables[taulu];
+            } catch (Exception ex) {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ IIO11300Vktehtavat/IIO13200-ADONET-DEMO-master/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿//Koodannut ja testannut toimivaksi 6.3.2014 EsaSalmik
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using JAMK.ICT;
+using JAMK.ICT.Data;
+
+namespace JAMK.ICT.ADOBlanco {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+
+        private string ConnStr;
+        private string TableName;
+        private DataTable dt;
+        private DataView dv;
+        public MainWindow() {
+            InitializeComponent();
+            IniMyStuff();
+        }
+
+        private void IniMyStuff() {
+            //TODO täytetään combobox asiakkaitten maitten nimillä
+            //esimerkki kuinka App.Configissa oleva connectionstring luetaan
+            string message = "";   
+            try {
+                ConnStr = JAMK.ICT.Properties.Settings.Default.Tietokanta;
+                lbMessages.Content = ConnStr;
+                TableName = JAMK.ICT.Properties.Settings.Default.Taulu;
+                dt = DBPlacebo.GetAllCustomersFromSQLServer(ConnStr, TableName, "", out message);
+                dv = dt.DefaultView;
+                FillMyCombo();
+            } catch (Exception ex) {
+
+                lbMessages.Content = ex.Message;
+            }
+        }
+        private void FillMyCombo() {
+            //cbCountries.ItemsSource = DBPlacebo.GetCitiesFromSQLServer(ConnStr, TableName).DefaultView;
+            //cbCountries.DisplayMemberPath = "city";
+            //cbCountries.SelectedValuePath = "city";
+            var result = (from c in dt.AsEnumerable()
+                          select c.Field<string>("City")).Distinct().ToList();
+            cbCountries.ItemsSource = result;
+        }
+
+        private void btnGet3_Click(object sender, RoutedEventArgs e) {
+            dgCustomers.ItemsSource = DBPlacebo.GetTestCustomers().DefaultView;
+        }
+
+        private void btnGetAll_Click(object sender, RoutedEventArgs e) {
+            string message = "";
+            try {
+                dgCustomers.ItemsSource = dt.DefaultView;
+            } catch (Exception ex) {
+                message = ex.Message;
+            } finally {
+                lbMessages.Content = message;
+            }
+        }
+
+        private void btnGetFrom_Click(object sender, RoutedEventArgs e) {
+            //TODO
+        }
+
+        private void btnYield_Click(object sender, RoutedEventArgs e) {
+            JAMK.ICT.JSON.JSONPlacebo2015 roskaa = new JAMK.ICT.JSON.JSONPlacebo2015();
+            MessageBox.Show(roskaa.Yield());
+        }
+
+        private void cbCountries_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            //SuodataVE1();
+            SuodataVE2();
+        }
+        private void SuodataVE1() {
+            string message = "";
+            string city = "";
+            try {
+                if (cbCountries.SelectedIndex >= 0) {
+                    city = cbCountries.SelectedValue.ToString();
+                    dgCustomers.ItemsSource = DBPlacebo.GetAllCustomersFromSQLServer(ConnStr, TableName, city, out message).DefaultView;
+                }
+            } catch (Exception ex) {
+                message = ex.Message;
+            } finally {
+                lbMessages.Content = message;
+            }
+        }
+        private void SuodataVE2() {
+            dv.RowFilter = string.Format("city = '{0}'", cbCountries.SelectedValue.ToString());
+        }
+    }
+}
